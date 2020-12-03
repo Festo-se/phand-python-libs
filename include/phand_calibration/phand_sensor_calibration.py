d68606a3b484ffdbd83a19caa26d32f9293c07d5
TSWA <timo.schwarzer@festo.com>
2020-12-03 08:09:20 +0100

    updated the version number

--- include/phand_calibration/phand_sensor_calibration.py
+++ include/phand_calibration/phand_sensor_calibration.py
@@ -1,4 +1,13 @@
-#!\usr\bin\env python3
+#!/usr/bin/env python3
+
+__author__ = "Marinus Matthias Moerdijk & Timo Schwarzer"
+__copyright__ = "Copyright 2020, Festo Coperate Bionic Projects"
+__credits__ = ["Timo Schwarzer", "Marinus Matthias Moerdijk"]
+__license__ = "GNU GPL v3.0"
+__version__ = "1.0.6"
+__maintainer__ = "Timo Schwarzer"
+__email__ = "timo.schwarzer@festo.com"
+__status__ = "Experimental"
 
 class PhandSensorCalibrationValue:
 
